78c8bb4154e3bb883c3e11d9e077059c2637352b
ZLWedge <zlapstun@gmail.com>
2014-12-22 16:49:45 +1100

    Finished problem 2 of Project Euler.

---
+++ ProjectEuler/Problem2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main(){
+	int a = 1;
+	int b = 2;
+	int c = 0;
+	int total = 0;
+	
+	while (b<4000000){
+	
+			c=a+b;
+			if (b % 2 == 0){
+	            total += c
+			a=b;
+			b=c;
+			}
+	}
+
+
+	std::cout << total;
+	return 0;
+}
